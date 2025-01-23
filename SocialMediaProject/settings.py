0dfe1f9cd2a6ceca74be1456cd24011bc3bb6b2c
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-23 15:53:09 +0530

    setup Postgresql for data

--- SocialMediaProject/settings.py
+++ SocialMediaProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SocialMediaProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SocialMediaClone',
+        'USER': 'postgres',
+        'PASSWORD': 'aftab@14581',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
