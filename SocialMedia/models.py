19b20806bf4f32cd5e66671731f10f7079e9a412
zafaraftab1 <zafaraftab14581@gmail.com>
2025-01-24 01:13:24 +0530

    Create a create POST page and show data in home page

--- SocialMedia/models.py
+++ SocialMedia/models.py
@@ -5,10 +5,13 @@ from django.db import models
 # Create your models here.
 class Post_models(models.Model):
     title = models.CharField(max_length=120)
-    image = models.ImageField(upload_to='SocialMediaCategory/')
     body = models.TextField()
+    image = models.ImageField(upload_to='SocialMediaCategory/')
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(max_length=120,default=uuid.uuid4,editable=False,unique=True,primary_key=True)
 
     def __str__(self):
         return self.title
+
+class Meta:
+    ordering = ['-created']
--- SocialMedia/views.py
+++ SocialMedia/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-
+from django.forms import ModelForm, forms
+from django.shortcuts import render,redirect
+from django import forms
 from SocialMedia.models import *
-
+from django.forms import ModelForm
 
 # Create your views here.
 def home(request):
     posts=Post_models.objects.all()
     return render(request, 'myHome/home.html', {'posts': posts})
 
+class PostCreateForm(ModelForm):
+    class Meta:
+        model = Post_models
+        fields = '__all__'
+        labels={
+            'body':'Captions'
+        }
+        widgets={
+            'captions':forms.Textarea(attrs={'rows':3,'placeholder':'Add a caption ...' ,'class':'font1 text-4xl'}),
+        }
+
 def postCreated(request):
-    return render(request, 'layout/created_post.html')
+    form = PostCreateForm()
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('home')
+    return render(request, 'layout/created_post.html', {'form': form})
